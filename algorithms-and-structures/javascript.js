73fca9014e9d5206a091ac0b8d839df8d7b1374d
Kyle Santowski <kyle@heartofstoneclothing.com>
2023-12-02 00:16:20 +0200

    ok

---
+++ algorithms-and-structures/javascript.js
@@ -0,0 +1,288 @@
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+
+// 01
+var myName;
+
+// 02
+var a;
+a = 7;
+
+// 03
+var b;
+b = 5;
+var c;
+c = b;
+
+// 04
+var a = 9;
+
+// 05
+var myFirstName;
+var myFirstName = "Kyle";
+var myLastName;
+var myLastName = "Santowski";
+
+// 06
+var a;
+a = 5;
+var b;
+b = 10;
+var c;
+
+c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 07
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 08
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// 09
+const FCC = "freeCodeCamp";
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact);
+
+// 10
+const sum = 10 + 10;
+
+// 11
+const difference = 45 - 33;
+
+// 12
+const product = 8 * 10;
+
+// 13
+const quotient = 66 / 33;
+
+// 14
+let myVar = 87;
+myVar++;
+
+// 15
+let myVar = 11;
+myVar--;
+
+// 16
+const ourDecimal = 5.7;
+let myDecimal = 5.7;
+
+// 17
+const product = 2.0 * 2.5;
+
+// 18
+const quotient = 4.4 / 2.0;
+
+// 19
+const remainder = 11%3;
+
+// 20
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+// 21
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 22
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// 23
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 24
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// 25
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// 26
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+// 27
+const myStr = "This is the start. " + "This is the end.";
+
+// 28
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+// 29
+const myName = "Kyle Santowski";
+const myStr ="My name is" + myName + "and I am well!";
+
+// 30
+const someAdjective = "brilliant";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// 31
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+// 32
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+
+// 33
+let myStr = "Jello World";
+
+myStr = "Hello World";
+
+// 34
+const lastName = "Lovelace";
+
+const thirdLetterOfLastName = lastName[2];
+
+// 35
+const lastName = "Lovelace";
+
+const lastLetterOfLastName = lastName[lastName.length - 1];
+
+// 36
+const lastName = "Lovelace";
+
+const secondToLastLetterOfLastName = lastName[lastName.length -2];
+
+// 37
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "I " + myVerb + " " + myAdverb + " from the " + myAdjective + " " + myNoun + ".";
+
+// 38
+const myArray = ["I'm Kyle"];
+myArray.push(1993);
+
+// 39
+const myArray = [["myArray"]];
+
+// 40
+const myArray = [50, 60, 70];
+const myData = myArray[0];
+
+// 41
+// modify array with indexes
+
+// 41
+
+
+// 42
+
+
+// 43
+
+
+// 44
+
+
+// 45
+
+
+// 46
+
+
+// 47
+
+
+// 48
+
+
+// 49
+
+
+// 50
+
+
+// 51
+
+
+// 52
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
