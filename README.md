540db550f226c87c9d48c228cd304064231e7060
Kyle Santowski <kyle@heartofstoneclothing.com>
2023-12-07 00:58:32 +0000

    errors are common nature when learning

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [<sup>🌐</sup>ur company](https://heartofstoneclothing.com).
+# Our Company(https://heartofstoneclothing.com).
 
 ![_0bc63358-a0b4-4aa0-8d6e-7b064791323c](https://github.com/heartofstoneclothing/heartofstoneclothing/assets/124478163/d2da518a-f685-46a4-9d55-de6b76b427d2) 
 
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+class CurrencyConverter:
+    def __init__(self, exchange_rates):
+        self.exchange_rates = exchange_rates
+
+    def convert(self, amount, from_currency, to_currency):
+        if from_currency not in self.exchange_rates or to_currency not in self.exchange_rates:
+            return None
+
+        conversion_rate = self.exchange_rates[to_currency] / self.exchange_rates[from_currency]
+        converted_amount = amount * conversion_rate
+
+        return converted_amount
+
+# Define your exchange rates
+exchange_rates = {
+    "USD": 1.0,
+    "EUR": 0.85,
+    "GBP": 0.75,
+    "JPY": 110.0,
+}
+
+# Create an instance of CurrencyConverter
+converter = CurrencyConverter(exchange_rates)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+
+    if request.method == "POST":
+        amount = float(request.form["amount"])
+        from_currency = request.form["from_currency"]
+        to_currency = request.form["to_currency"]
+
+        result = converter.convert(amount, from_currency, to_currency)
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
